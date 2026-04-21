0fa479e442ca11a96aaf248e8b62fb5a6c7759cf
Vitaly Andrev <v.andreev@systtech.ru>
2026-04-21 21:58:24 +0700

    feature: implement user-derived path selection

---
+++ Tests/resolve/Get-RunnerPaths.Tests.ps1
@@ -0,0 +1,33 @@
+BeforeAll {
+    . "$PSScriptRoot\..\..\hyper-v\ubuntu\resolve\Get-RunnerPaths.ps1"
+}
+
+Describe 'Get-RunnerPaths' {
+
+    Context 'path construction' {
+        It 'derives CacheDir from the runner user home directory' {
+            $paths = Get-RunnerPaths -RunnerUser 'u-runner' -RunnerVersion '2.317.0'
+            $paths.CacheDir | Should -Be '/home/u-runner/cache'
+        }
+
+        It 'constructs TarName from the version' {
+            $paths = Get-RunnerPaths -RunnerUser 'u-runner' -RunnerVersion '2.317.0'
+            $paths.TarName | Should -Be 'actions-runner-linux-x64-2.317.0.tar.gz'
+        }
+
+        It 'constructs TarPath as CacheDir joined with TarName' {
+            $paths = Get-RunnerPaths -RunnerUser 'u-runner' -RunnerVersion '2.317.0'
+            $paths.TarPath | Should -Be '/home/u-runner/cache/actions-runner-linux-x64-2.317.0.tar.gz'
+        }
+
+        It 'constructs RunnerDir when RunnerName is provided' {
+            $paths = Get-RunnerPaths -RunnerUser 'u-runner' -RunnerVersion '2.317.0' -RunnerName 'runner-a'
+            $paths.RunnerDir | Should -Be '/home/u-runner/runners/runner-a'
+        }
+
+        It 'returns null RunnerDir when RunnerName is omitted' {
+            $paths = Get-RunnerPaths -RunnerUser 'u-runner' -RunnerVersion '2.317.0'
+            $paths.RunnerDir | Should -BeNullOrEmpty
+        }
+    }
+}
