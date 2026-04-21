75b9afecd11aab81d6a85fc0242b0c7b1cb215ba
Vitaly Andrev <v.andreev@systtech.ru>
2026-04-21 18:46:26 +0700

    feature: implement reading config from secrets

---
+++ Tests/Read-GitHubRunnersConfig.Tests.ps1
@@ -0,0 +1,44 @@
+BeforeAll {
+    function Get-InfrastructureSecret         { param($VaultName, $SecretName) }
+    function ConvertFrom-GitHubRunnersConfigJson { param($Json) }
+
+    . "$PSScriptRoot\..\hyper-v\ubuntu\resolve\Read-GitHubRunnersConfig.ps1"
+}
+
+Describe 'Read-GitHubRunnersConfig' {
+
+    Context 'vault read' {
+        It 'calls Get-InfrastructureSecret with the correct vault and secret names' {
+            Mock Get-InfrastructureSecret { '[]' }
+            Mock ConvertFrom-GitHubRunnersConfigJson { }
+            Read-GitHubRunnersConfig
+            Should -Invoke Get-InfrastructureSecret -Times 1 -ParameterFilter {
+                $VaultName -eq 'GitHubRunners' -and $SecretName -eq 'GitHubRunnersConfig'
+            }
+        }
+
+        It 'passes the vault JSON to ConvertFrom-GitHubRunnersConfigJson' {
+            Mock Get-InfrastructureSecret { '["sentinel"]' }
+            Mock ConvertFrom-GitHubRunnersConfigJson { }
+            Read-GitHubRunnersConfig
+            Should -Invoke ConvertFrom-GitHubRunnersConfigJson -Times 1 -ParameterFilter {
+                $Json -eq '["sentinel"]'
+            }
+        }
+
+        It 'returns the entries from ConvertFrom-GitHubRunnersConfigJson' {
+            Mock Get-InfrastructureSecret { '[]' }
+            Mock ConvertFrom-GitHubRunnersConfigJson {
+                [PSCustomObject]@{ runnerName = 'ubuntu-01-ci' }
+            }
+            $result = @(Read-GitHubRunnersConfig)
+            $result | Should -HaveCount 1
+            $result[0].runnerName | Should -Be 'ubuntu-01-ci'
+        }
+
+        It 'propagates errors from Get-InfrastructureSecret' {
+            Mock Get-InfrastructureSecret { throw 'vault unavailable' }
+            { Read-GitHubRunnersConfig } | Should -Throw -ExpectedMessage '*vault unavailable*'
+        }
+    }
+}
