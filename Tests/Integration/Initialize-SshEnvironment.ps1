8205beb414cbfd6f88d3ae9efad9d5e80d7cb3e7
Vitaly Andrev <v.andreev@systtech.ru>
2026-04-30 10:17:39 +0700

    chore: up req verions

--- Tests/Integration/Initialize-SshEnvironment.ps1
+++ Tests/Integration/Initialize-SshEnvironment.ps1
@@ -123,7 +123,7 @@ Start-Sleep -Seconds 1
 # -----------------------------------------------------------------------
 
 Write-Step 5 'installing Infrastructure.Common'
-Install-Module Infrastructure.Common -MinimumVersion '1.3.3' `
+Install-Module Infrastructure.Common -MinimumVersion '2.0.0' `
     -Scope CurrentUser -Force -SkipPublisherCheck
 Import-Module Infrastructure.Common -Force -ErrorAction Stop
 
